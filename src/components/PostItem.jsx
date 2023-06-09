44a0464c1ed7443e8a46e110317894f3e58483da
Depthmane <depthmane@yandex.ru>
2023-06-09 20:19:38 +0300

    initialised a project

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MyButton from "./ UI/button/MyButton";
+
+const PostItem = (props) => {
+    return (
+        <div className="post">
+            <strong>{props.number}. {props.post.title}</strong>
+            <div>
+                {props.post.body}
+            </div>
+            <div className="post_bttns">
+                <MyButton onClick={()=> props.remove(props.post)}>Удалить пост</MyButton>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
